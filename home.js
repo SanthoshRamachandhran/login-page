fd8263a2946f487e4962edf60b56a1e9ae76bbd3
SanthoshRamachandhran <ramsanthosh1913@gmail.com>
2024-11-12 15:15:52 +0530

    second commit

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
 console.log("hello world")
-git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
+// git config --global user.email "you@example.com"
+//   git config --global user.name "Your Name"
+console.log("hello world1")
